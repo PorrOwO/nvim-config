9ec930179896f91a3d48a6cf63d84853ed658286
PorrOwO <andrea.dalcin04@gmail.com>
2025-02-28 15:32:55 +0100

    intial commit

---
+++ init.lua
@@ -0,0 +1,4 @@
+vim.g.zig_fmt_autosave = 0
+require("hanoi.maps")
+require("hanoi.settings")
+require("hanoi.lazy")
---
+++ lua/hanoi/plugins/snacks.lua
@@ -0,0 +1,19 @@
+-- lazy.nvim
+return {
+  "folke/snacks.nvim",
+  ---@type snacks.Config
+  opts = {
+    dashboard = {
+      preset = {
+       sections = {
+        { section = "header" },
+        { section = "keys", gap = 1, padding = 1 },
+        { section = "startup" },
+      },
+      -- your dashboard configuration comes here
+      -- or leave it empty to use the default settings
+      -- refer to the configuration section below
+    }
+  }
+}
+}
