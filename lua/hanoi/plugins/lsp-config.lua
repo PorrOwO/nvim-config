b0c792c677651e465731e8f6afac47be4f80f062
PorrOwO <andrea.dalcin04@gmail.com>
2025-08-09 18:03:06 +0200

    fixed lsp not working in non ruby languages

--- lua/hanoi/plugins/lsp-config.lua
+++ lua/hanoi/plugins/lsp-config.lua
@@ -1,4 +1,5 @@
-return {
+return 
+{
   {
     'williamboman/mason.nvim',
     lazy = false,
@@ -9,7 +10,13 @@ return {
   {
     'williamboman/mason-lspconfig.nvim',
     lazy = false,
-    opts = {
+    opts = { 
+      handlers = {
+        function(server_name)
+          require('lspconfig')[server_name].setup({})
+        end,
+      },
+
       ensure_installed = {'ruby_lsp'},
       automatic_installation = true,
     },
@@ -21,29 +28,30 @@ return {
     cmd = {'LspInfo', 'LspInstall', 'LspStart'},
     config = function()
       local capabilities = require('cmp_nvim_lsp').default_capabilities()
-      
+
       local lspconfig = require('lspconfig')
       lspconfig.ruby_lsp.setup({
         capabilities = capabilities,
       })
       -- LspAttach is where you enable features that only work
       -- if there is a language server active in the file
-      --vim.api.nvim_create_autocmd('LspAttach', {
-      --  desc = 'LSP actions',
-      --  callback = function(event)
-      --    local opts = {buffer = event.buf}
+      vim.api.nvim_create_autocmd('LspAttach', {
+        desc = 'LSP actions',
+        callback = function(event)
+          local opts = {buffer = event.buf}
 
-      vim.keymap.set('n', 'K', '<cmd>lua vim.lsp.buf.hover()<cr>', opts)
-      vim.keymap.set('n', 'gd', '<cmd>lua vim.lsp.buf.definition()<cr>', opts)
-      vim.keymap.set('n', 'gD', '<cmd>lua vim.lsp.buf.declaration()<cr>', opts)
-      vim.keymap.set('n', 'gi', '<cmd>lua vim.lsp.buf.implementation()<cr>', opts)
-      vim.keymap.set('n', 'go', '<cmd>lua vim.lsp.buf.type_definition()<cr>', opts)
-      vim.keymap.set('n', 'gr', '<cmd>lua vim.lsp.buf.references()<cr>', opts)
-      vim.keymap.set('n', 'gs', '<cmd>lua vim.lsp.buf.signature_help()<cr>', opts)
-      vim.keymap.set('n', '<F2>', '<cmd>lua vim.lsp.buf.rename()<cr>', opts)
-      vim.keymap.set({'n', 'x'}, '<F3>', '<cmd>lua vim.lsp.buf.format({async = true})<cr>', opts)
-      vim.keymap.set('n', '<F4>', '<cmd>lua vim.lsp.buf.code_action()<cr>', opts)
-    end,
-      --})
+          vim.keymap.set('n', 'K', '<cmd>lua vim.lsp.buf.hover()<cr>', opts)
+          vim.keymap.set('n', 'gd', '<cmd>lua vim.lsp.buf.definition()<cr>', opts)
+          vim.keymap.set('n', 'gD', '<cmd>lua vim.lsp.buf.declaration()<cr>', opts)
+          vim.keymap.set('n', 'gi', '<cmd>lua vim.lsp.buf.implementation()<cr>', opts)
+          vim.keymap.set('n', 'go', '<cmd>lua vim.lsp.buf.type_definition()<cr>', opts)
+          vim.keymap.set('n', 'gr', '<cmd>lua vim.lsp.buf.references()<cr>', opts)
+          vim.keymap.set('n', 'gs', '<cmd>lua vim.lsp.buf.signature_help()<cr>', opts)
+          vim.keymap.set('n', '<F2>', '<cmd>lua vim.lsp.buf.rename()<cr>', opts)
+          vim.keymap.set({'n', 'x'}, '<F3>', '<cmd>lua vim.lsp.buf.format({async = true})<cr>', opts)
+          vim.keymap.set('n', '<F4>', '<cmd>lua vim.lsp.buf.code_action()<cr>', opts)
+        end 
+      })
+    end
   }
 }
