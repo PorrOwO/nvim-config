315fe883b02018b086a7dcf2a8e344f7fd3088aa
PorrOwO <andrea.dalcin04@gmail.com>
2026-01-14 18:51:19 +0100

    add theme picker

--- lua/hanoi/plugins/colorscheme.lua
+++ lua/hanoi/plugins/colorscheme.lua
@@ -1,7 +1,29 @@
-return { 
-    "miikanissi/modus-themes.nvim", 
-    priority = 1000,
-    config = function()
-        vim.cmd("colorscheme modus")
-    end
+return {
+    { 
+        "miikanissi/modus-themes.nvim", 
+        lazy = false,
+    },
+    {
+        'maxmx03/solarized.nvim',
+        lazy = false,
+    },
+
+    { 
+        "calind/selenized.nvim",
+        lazy = false,
+    },
+    {
+        "blazkowolf/gruber-darker.nvim",
+        lazy = false,
+    },
+    {
+        "zaldih/themery.nvim",
+        lazy = false,
+        config = function()
+            require("themery").setup({
+                themes = {"modus_operandi", "modus_vivendi", "solarized", "selenized", "gruber-darker"},
+                livePreview = true,
+            })
+        end
+    }
 }
