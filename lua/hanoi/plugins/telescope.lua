4d0bf8f4f1f01fd20649bc725724b543cfb1ad4a
PorrOwO <andrea.dalcin04@gmail.com>
2026-04-08 10:51:30 +0200

    update for writing lisp lol

--- lua/hanoi/plugins/telescope.lua
+++ lua/hanoi/plugins/telescope.lua
@@ -1,12 +1,26 @@
 return {
-    'nvim-telescope/telescope.nvim', tag = '0.1.8',
--- or                              , branch = '0.1.x',
-      dependencies = { 'nvim-lua/plenary.nvim' },
+    'nvim-telescope/telescope.nvim', version = '*',
+    dependencies = {
+        'nvim-lua/plenary.nvim',
+        -- optional but recommended
+        { 'nvim-telescope/telescope-fzf-native.nvim', build = 'make' },
+    },
 
-      keys = {
+    keys = {
         { '<leader>ff', "<cmd>Telescope find_files<CR>", { desc = 'Telescope find files' }},
         {'<leader>fg',"<cmd>Telescope live_grep<CR>", { desc = 'Telescope live grep' }},
         {'<leader>fb',"<cmd>Telescope buffers<CR>", { desc = 'Telescope buffers' }},
         { '<leader>fh',"<cmd>Telescope help_tags<CR>", { desc = 'Telescope help tags' }},
-      }
-    }
+    },
+
+    require('telescope').setup{
+        pickers = {
+            find_files = {
+                theme = "ivy"
+            },
+            live_grep = {
+                theme = "ivy"
+            },
+        },
+    },
+}
